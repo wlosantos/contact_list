3d25ab2318f51fdf50d0652ea09970becaae08f5
Wendel Lopes <wendelopes@msn.com>
2020-04-04 11:45:21 -0300

    Backup

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+
+    def index
+        @nome = 'Wendel Lopes dos Santos'
+    end
+
+    def sobre
+
+    end
+
+    def contato
+
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+    root to: 'static_pages#index'
+    get 'sobre', to: 'static_pages#sobre'
+    get 'contato', to: 'static_pages#contato'
+
+    resources :contacts
+    resources :users, only: [:new, :create]
+end
